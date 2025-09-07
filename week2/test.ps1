4c383d3c0aea89fa33c2904e5737724279cce290
natekreit <nathan.kreit@mymail.champlain.edu>
2025-09-07 09:27:53 -0400

    added code assignment

---
+++ week2/test.ps1
@@ -0,0 +1,15 @@
+﻿#Part 1
+Get-Process | Where-Object { $_.ProcessName -ilike 'C*' } | Select-Object ProcessName, Id
+
+#Part 2
+Get-Process | Where-Object { $_.Path -and $_.Path -notmatch 'system32' } | Select-Object ProcessName, Id, Path
+
+#Part 3
+Get-Service | Where-Object { $_.Status -eq 'Stopped' } | Sort-Object -Property Name | Select-Object Name, Status | Export-Csv -Path "Stop_Service"
+
+#Part 4
+if (Get-Process -Name chrome -ErrorAction SilentlyContinue) {
+    Stop-Process -Name chrome -Force
+} else {
+    Start-Process chrome "https://www.champlain.edu"
+}
