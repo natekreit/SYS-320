85d2c4fde7d1d7aad7bbaa60d57edf2eb501129d
natekreit <nathan.kreit@mymail.champlain.edu>
2025-09-23 20:42:49 -0400

    update

--- week5/GatherClasses.ps1
+++ week5/GatherClasses.ps1
@@ -1,3 +1,4 @@
+
 #Write a Powershsell function that Scrapes the table from the web page and saves the content to a custom object
 
 function gatherClasses(){
@@ -29,3 +30,38 @@ for($i=1; $i -lt $trs.length; $i++){ #Going over every tr element
 }
 return $FullTable
 }
+
+
+#Change days into the full word
+function daysTranslator($FullTable){
+
+#Go over every record in the table
+for($i=0; $i -lt $FullTable.length; $i++){
+
+    #Empty array to hold days for every record
+    $Days = @()
+
+    #If you see "M" -> Monday
+    if($FullTable[$i].$Days -ilike "M"){ $Days += "Monday" }
+
+    #If you see "T" followed by T, W, or F -> Tuesday
+    if($FullTable[$i].$Days -ilike "*T[TWF]*"){ $Days += "Tuesday" }
+    #If you only see "T" -> Tuesday
+    ElseIf($FullTable[$i].$Days -ilike "T"){ $Days += "Tuesday" }
+
+    #If you see "W" -> Wednesday
+    if($FullTable[$i].$Days -ilike "W"){ $Days += "Wednesday" }
+
+    #If you see "TH" -> Thursday
+    if($FullTable[$i].$Days -ilike "TH"){ $Days += "Thursday" }
+
+    #If you see "F" -> Friday
+    if($FullTable[$i].$Days -ilike "F"){ $Days += "Friday" }
+
+    #Make the switch
+    $FullTable[$i].$Days = $Days
+}
+
+return $FullTable
+}
+daysTranslator
