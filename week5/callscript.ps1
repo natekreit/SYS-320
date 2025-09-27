62855e572f1b6c8f495430a696e01aa70ec0f44f
natekreit <nathan.kreit@mymail.champlain.edu>
2025-09-27 15:29:35 -0400

    update

--- week5/callscript.ps1
+++ week5/callscript.ps1
@@ -12,4 +12,5 @@ clear
 #List all the classes of JOYC 310 on Mondays, only displat Class Code and Times
 #Sort by Start time
 $FullTable | Where-Object { ($_.Location -ilike "JOYC 310") -and ($_.days -ilike "Monday") } | `
-             Sort-Object "Time Start
+             Sort-Object "Time Start" | `
+             Select-Object "Time Start", "Time End", "Class Code"
